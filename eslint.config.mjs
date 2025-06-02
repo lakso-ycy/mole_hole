26e69bc79345188f84be304cc4beea092b94d962
Arkaannnn <hilmi.nizaar7@gmail.com>
2025-06-02 19:04:49 +0700

    update eslint untuk bisa membaca cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,28 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["cypress/**/*.js"], // khusus untuk file Cypress
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,       // untuk describe, it, beforeEach, dll
+        Cypress: "readonly",    // untuk Cypress class
+        cy: "readonly",         // untuk cy object
+      },
+    },
+  },
 ]);
