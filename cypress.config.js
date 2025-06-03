ae8db730942b2ee8b97e54caa08b6504538e569e
Arkaannnn <hilmi.nizaar7@gmail.com>
2025-06-03 15:56:37 +0700

    push arkaan

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // config lainnya...
-      return config; // pastikan config dikembalikan jika dimodifikasi
-    },
+    setupNodeEvents() {},
   },
   reporter: "junit",
   reporterOptions: {
     mochaFile: "results/cypress-results-[hash].xml",
-    toConsole: false,
-  },
+    toConsole:false,
+},
 });
