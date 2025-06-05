18efa2faef5394461e49362b2e90b8164eb337cb
Arkaannnn <hilmi.nizaar7@gmail.com>
2025-06-05 10:18:37 +0700

    iki seng bener

--- script.js
+++ script.js
@@ -470,12 +470,6 @@ if (resetScoresBtn) {
   resetScoresBtn.addEventListener("click", resetScores);
 }
 
-// Event listener for the reset scores button
-const resetScoresBtn = document.getElementById("reset-scores-btn");
-if (resetScoresBtn) {
-  resetScoresBtn.addEventListener("click", resetScores);
-}
-
 document.addEventListener('mousedown', () => {
   document.body.classList.add('hammer-hit.png');
 });
